f50f1e1fe6fa8e4e14848c4d2c8b52e610a7261d
Ramon Asuncion <asuncionbatista@gmail.com>
2020-07-16 09:56:32 -0400

    working smooth scroll for button

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,15 +1,13 @@
 //testing out JS for smooth scrolling
-$('.contain-project a').on(
-	'click',
-	function (e) {
-		console.log(this.has);
-		if (this.has !== '') {
-			e.preventDefault();
-			const hash = this.hash;
-			$('html, body').animate({
+$('.contain-project a').on('click', function (e) {
+	if (this.has !== '') {
+		e.preventDefault();
+		const hash = this.hash;
+		$('html, body').animate(
+			{
 				scrollTop: $(hash).offset().top,
-			});
-		}
-	},
-	600
-);
+			},
+			800
+		);
+	}
+});
