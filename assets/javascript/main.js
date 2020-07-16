ccce24fb74cdd002557e226a4cadce2cd2972dee
Ramon Asuncion <asuncionbatista@gmail.com>
2020-07-16 19:57:17 -0400

    fixed main.js scroll

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,13 +1,18 @@
 //smooth scrolling
-$('.contain-project a').on('click', function (e) {
-	if (this.has !== '') {
-		e.preventDefault();
-		const hash = this.hash;
-		$('html, body').animate(
-			{
-				scrollTop: $(hash).offset().top,
-			},
-			800
-		);
-	}
+$(document).ready(function () {
+	// ^ waits until the document is "ready" to do this function
+	$('.contain-project a').on('click', function (e) {
+		if (this.has !== '') {
+			e.preventDefault();
+			const hash = this.hash;
+			$('html, body').animate(
+				{
+					scrollTop: $(hash).offset().top,
+				},
+				800 //scroll speed
+			);
+		}
+	});
 });
+
+//understand what each line does
