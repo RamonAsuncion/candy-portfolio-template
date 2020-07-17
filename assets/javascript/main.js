8f94ae4d16425b203a556d096b0bb7be5d130799
Ramon Asuncion <asuncionbatista@gmail.com>
2020-07-17 01:38:26 -0400

    added what each means

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,18 +1,34 @@
 //smooth scrolling
+
 $(document).ready(function () {
 	// ^ waits until the document is "ready" to do this function
 	$('.contain-project a').on('click', function (e) {
-		if (this.has !== '') {
+		//$(selector).action()
+		if (this.hash !== '') {
+			//if its not equal to an empty string
 			e.preventDefault();
+			//prevents the default behavior
 			const hash = this.hash;
+			//hash value into a variable
 			$('html, body').animate(
+				//targets the body and calls the animate method
 				{
 					scrollTop: $(hash).offset().top,
+					//takes an object and adds the property scroll Top (scrolls from the top)
+					//takes the has value and call the offset method and top
 				},
-				800 //scroll speed
+				800 //speed of the scroll
 			);
 		}
 	});
 });
 
-//understand what each line does
+$(document).ready(function () {
+	$('footer a').on('click', function (e) {
+		if (this.hash !== '') {
+			e.preventDefault();
+			const hash2 = this.hash;
+			$('html, body').animate({}, 800);
+		}
+	});
+});
