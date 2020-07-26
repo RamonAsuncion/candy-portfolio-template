eec046620cdb63a9350ca43c548c518491a19090
Ramon <asuncionbatista@gmail.com>
2020-07-25 23:51:37 -0400

    added text to main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -27,8 +27,11 @@ $(document).ready(function () {
 	$('footer a').on('click', function (e) {
 		// console.log(this.hash); #top hash (console)
 		if (this.hash !== '') {
+			//checks if its not equal to an empty string
 			e.preventDefault();
+			//prevents the default behavior
 			const hash2 = this.hash;
+			//hash value into a variable
 			$('html, body').animate(
 				{
 					scrollTop: 0, //0 is the top position
