aacd2d8995ee05ddd95431ff1ef06193394427a1
Ramon Asuncion <asuncionbatista@gmail.com>
2021-12-21 08:24:50 -0500

    Responsiveness on mobile.

---
+++ assets/javascript/smooth-scrolling.js
@@ -0,0 +1,31 @@
+//smooth scrolling
+
+$(document).ready(function () {
+	$('.contain-project a').on('click', function (e) {
+		if (this.hash !== '') {
+			e.preventDefault();
+			const hash = this.hash;
+			$('html, body').animate(
+				{
+					scrollTop: $(hash).offset().top,
+				},
+				900 
+			);
+		}
+	});
+});
+
+$(document).ready(function () {
+	$('footer a').on('click', function (e) {
+		if (this.hash !== '') {
+			e.preventDefault();
+			const hash2 = this.hash;
+			$('html, body').animate(
+				{
+					scrollTop: 0, //$(hash2).offset().top,
+				},
+				900 
+			);~
+		}
+	});
+});
